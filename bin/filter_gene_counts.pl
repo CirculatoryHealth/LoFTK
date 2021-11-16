99b6ee5849843f471ae3bf6522ef5c9236fab901
Abdulrahman Alasiri <ai7alasiri@gmail.com>
2021-11-17 00:25:37 +0100

    filter_counts

---
+++ bin/filter_gene_counts.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use strict;
+use warnings;
+use autodie;
+use List::MoreUtils qw(indexes);
+
+my $usage = "$0 ids counts";
+my $ids = shift or die $usage;
+my $counts = shift or die $usage;
+
+open my $iin, '<', $ids;
+my %h = map { chomp; s/^\s+|\s+$//; $_ => 1 } <$iin>;
+close $iin;
+
+open my $vin, '<', $counts;
+my @indexes;
+
+while (my $line = <$vin>) {
+    chomp $line;
+    next if $line =~ /^##/;
+	my @f = split /\s+/, $line;
+
+    if ($f[0] eq 'gene_ID') {
+	for my $k (keys %h) {
+	    push @indexes, indexes { $_ =~ /$k/ } @f;
+	}
+    }
+
+    if (@indexes) {
+        say join "\t", @f[0..1,@indexes];
+    }
+    else {
+	say "No LoFs from $ids found in $counts.";
+	last;
+    }
+}
+close $vin;
